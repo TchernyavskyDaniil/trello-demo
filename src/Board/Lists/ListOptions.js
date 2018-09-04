45cd925aefbb82549defb1f5e68b243377906f11
Tchernyavsky <tchernyavsky.daniil@yandex.ru>
2018-09-04 19:05:42 +0300

    fixed

--- src/Board/Lists/ListOptions.js
+++ src/Board/Lists/ListOptions.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import onClickOutside from "react-onclickoutside";
-import axios from "../../axios";
 import {
   Close,
   Container,
